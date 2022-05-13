5ac9d333add6925b662a267ab2fa53b4300811f8
TommyVanAcker <88966702+TommyVanAcker@users.noreply.github.com>
2022-05-13 22:48:44 +0200

    eerste setup en speellogica

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class NumberWizard : MonoBehaviour
 {
+    [SerializeField] TMPro.TextMeshProUGUI raadGetal;
+    [SerializeField] int minGetal = 1;
+    [SerializeField] int maxGetal = 1000;
+
+    int speelGetal;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("This is the Wizard");
+        speelGetal = Random.Range(minGetal, maxGetal + 1);
+        raadGetal.text = speelGetal.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    void RaadVolgende()
     {
-        
+        speelGetal = Random.Range(minGetal, maxGetal + 1);
+        raadGetal.text = speelGetal.ToString();
+    }
+
+   public void RaadHoger()
+    {
+        minGetal = speelGetal;
+        RaadVolgende();
+    }
+
+
+    public void RaadLager()
+    {
+        maxGetal = speelGetal;
+        RaadVolgende();
     }
 }
